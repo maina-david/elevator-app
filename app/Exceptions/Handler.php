3df402c54250e23a4fec66867338a8e01447cefd
maina-david <waihura.m@gmail.com>
2023-08-20 12:25:26 +0300

    Updated .env.example, app/Exceptions/Handler.php, and config/websockets.php.
    .env.example:
    - Updated APP_NAME to ElevatorApp
    - Updated DB_DATABASE to elevator_app
    
    app/Exceptions/Handler.php:
    - Added handling for UnauthorizedHttpException, AuthenticationException, NotFoundHttpException, ModelNotFoundException, and MethodNotAllowedHttpException.
    
    config/websockets.php:
    - Updated path to app-websockets

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,12 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -23,8 +28,40 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // Handling UnauthorizedHttpException (401)
+        $this->renderable(function (UnauthorizedHttpException $e, Request $request) {
+            return response()->json([
+                'message' => 'Unauthorized access.',
+            ], 401);
+        });
+
+        // Handling AuthenticationException (401)
+        $this->renderable(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'message' => 'Unauthorized access.',
+            ], 401);
+        });
+
+        // Handling NotFoundHttpException (404)
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            return response()->json([
+                'message' => 'Resource not found.',
+            ], 404);
+        });
+
+        // Handling ModelNotFoundException (404)
+        $this->renderable(function (ModelNotFoundException $e, Request $request) {
+            return response()->json([
+                'message' => 'Record not found.',
+            ], 404);
+        });
+
+        //Handling MethodNotAllowedHttpException(405)
+        $this->renderable(function (MethodNotAllowedHttpException $e, Request $request) {
+            return response()->json([
+                'status' => 405,
+                'message' => 'Method Not Allowed',
+            ], 405);
         });
     }
 }
