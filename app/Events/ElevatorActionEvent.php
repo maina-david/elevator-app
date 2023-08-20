edb8df4c0a62f1cae6dfa00da45853a17301cfe9
maina-david <waihura.m@gmail.com>
2023-08-20 16:46:15 +0300

    Improved the `broadcastAs()` method to return a more human-readable message that includes the elevator's current floor and state. Also added a new method to get the readable floor name.

--- app/Events/ElevatorActionEvent.php
+++ app/Events/ElevatorActionEvent.php
@@ -43,7 +43,72 @@ class ElevatorActionEvent implements ShouldBroadcast
      */
     public function broadcastAs()
     {
-        return 'Elevator action: ' . $this->elevatorLog->action;
+        $stateMappings = [
+            'idle' => 'Idle',
+            'moving' => 'Moving',
+            'stopped' => 'Stopped',
+            'doors_opening' => 'Doors Opening',
+            'doors_open' => 'Doors Open',
+            'doors_closing' => 'Doors Closing',
+            'doors_closed' => 'Doors Closed',
+        ];
+
+        $actionMappings = [
+            'call' => isset(json_decode($this->elevatorLog->details)->target_floor)
+                ? 'Elevator called to Floor ' . json_decode($this->elevatorLog->details)->target_floor
+                : 'Elevator called',
+            'idle' => 'Idle',
+            'moving' => 'Moving',
+            'stopped' => 'Stopped',
+            'doors_opening' => 'Doors Opening',
+            'doors_open' => 'Doors Open',
+            'doors_closing' => 'Doors Closing',
+            'doors_closed' => 'Doors Closed',
+        ];
+
+        $state = $this->elevatorLog->state;
+        $action = $this->elevatorLog->action;
+        $currentFloor = $this->elevatorLog->current_floor;
+        $elevatorBuilding = $this->elevatorLog->elevator->building ?? null;
+
+        if ($elevatorBuilding) {
+            $buildingFloors = $elevatorBuilding->number_of_floors;
+            $floorNames = [];
+
+            for ($i = 1; $i <= $buildingFloors; $i++) {
+                $floorNames[$i] = $this->getReadableFloorName($i);
+            }
+
+            // Use the mappings to get human-readable state and action names
+            $formattedState = $stateMappings[$state] ?? 'Unknown State';
+            $formattedAction = $actionMappings[$action] ?? 'Unknown Action';
+
+            // Determine the final message based on matching state and action
+            if ($formattedState === $formattedAction) {
+                $formattedMessage = "Elevator at {$floorNames[$currentFloor]} Floor: $formattedState";
+            } else {
+                $formattedMessage = "Elevator at {$floorNames[$currentFloor]} Floor: $formattedState - $formattedAction";
+            }
+
+            return $formattedMessage;
+        }
+
+        return 'Elevator Log Broadcast'; // Default message if building info is not available
+    }
+
+    /**
+     * Get the readable floor name (e.g., 1st, 2nd, 3rd, etc.).
+     *
+     * @param int $floorNumber
+     * @return string
+     */
+    private function getReadableFloorName($floorNumber)
+    {
+        $ordinalSuffix = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+        $suffixIndex = $floorNumber % 100;
+        $suffix = ($suffixIndex >= 11 && $suffixIndex <= 13) ? 'th' : $ordinalSuffix[$suffixIndex % 10];
+
+        return "{$floorNumber}{$suffix}";
     }
 
     /**
